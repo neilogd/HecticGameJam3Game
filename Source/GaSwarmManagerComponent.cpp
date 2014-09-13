0401b3d50c5bf45582424d6818ec09efd8d4d9ac
Daniel de Zwart <d>
2014-09-13 12:31:00 +1000

    Fixed some bugs in the swarm system

--- Source/GaSwarmManagerComponent.cpp
+++ Source/GaSwarmManagerComponent.cpp
@@ -138,13 +138,13 @@ MaVec2d GaSwarmManagerComponent::forceAwayFromNearbyUnits( GaSwarmElementCompone
 MaVec2d GaSwarmManagerComponent::forceTowardsNearbyUnits( GaSwarmElementComponentRef Unit, BcU8 UnitCount, BcU8 Mask )
 {
 	SwarmElementList elements = getNearbyUnits(Unit->getPosition(), UnitCount, Mask);
-	MaVec2d average;
+	MaVec2d average( 0.0f, 0.0f );
 	for ( BcU32 Idx = 0; Idx < elements.size(); ++Idx )
 	{
 		average += elements[Idx]->getPosition();
 	}
 	average = (average / (BcF32)elements.size());
-	return (Unit->getPosition() - average).normal();
+	return -(Unit->getPosition() - average).normal();
 }
 
 SwarmElementList GaSwarmManagerComponent::getNearbyUnits( MaVec2d Position, BcU8 UnitCount, BcU8 Mask )
